f52c4db3063aa657ee0d21cb0a6835245ac6375a
Sebastian B <sebastian.benavides.madrigal@est.una.ac.cr>
2024-03-09 11:01:13 -0600

    Fix

--- Patron Decorador/Patron Decorador/x64/include/Ingrediente.h
+++ Patron Decorador/Patron Decorador/x64/include/Ingrediente.h
@@ -2,7 +2,7 @@
 #include "Combo.h"
 
 class Ingrediente :public Combo{   //Esta es la clase mas importante
-								//Es el decorador (Grande)
+                                    // Es el decorador (Grande)
 protected:
 	string nombre;
 	Combo* combo;  //Puntero que enlaza las capas
--- Patron Decorador/Patron Decorador/x64/src/main.cpp
+++ Patron Decorador/Patron Decorador/x64/src/main.cpp
@@ -13,22 +13,78 @@ int main() {
 	//Creando Hamburguesas
 
 	int op=4;
+    Combo* combo = new Hamburguesa();
+    Combo* aux = nullptr;
+    cout << "Bienvenido a la hamburgueseria" << endl;
+
 	cout << "Creando hamburguesas" << endl;
 
 	//Tarea moral ----> CREAR UN MENU
 
 	do {
-	
+
 		cout << "Menu" << endl;
 		//switch (case)
 		//El usuario pida lo que quiera
+        cout << "Seleccione un ingrediente" << endl;
+        cout << "1. Tomate" << endl;
+        cout << "2. Torta" << endl;
+        cout << "3. Queso" << endl;
+        cout << "4. Lechuga" << endl;
+        cout << "5. Salir" << endl;
+        cin >> op;
+        switch (op) {
+        case 1:
+            if(aux != nullptr){
+                Combo* tomate = new Tomate(aux);
+                aux = tomate;
 
-	} while (op != 4);
-		//Tambien poder quitar algun ingrediente que el usuariio no le guste
+            }else {
+                Combo* tomate = new Tomate(combo);
+                aux = tomate;
+            }
+            break;
+        case 2:
+            if (aux != nullptr) {
+                Combo* torta = new Torta(aux);
+                aux = torta;
+            }else {
+                Combo* torta = new Torta(combo);
+                aux = torta;
+            }
+            break;
+        case 3:
+            if (aux != nullptr) {
+                Combo* queso = new Queso(aux);
+                aux = queso;
+            }else {
+                Combo* queso = new Queso(combo);
+                aux = queso;
+            }
+            break;
+        case 4:
+            if (aux != nullptr) {
+                Combo* lechuga = new Lechuga(aux);
+                aux = lechuga;
+            }else {
+                Combo* lechuga = new Lechuga(combo);
+                aux = lechuga;
+            }
+            break;
+        case 5:
+            cout << "Saliendo" << endl;
+            cout << "Hamburguesa: " << aux->costo() << " Colones" << endl;
+            break;
+        default:
+            cout << "Opcion no valida" << endl;
+            break;
+        }
 
-		//combo basico
+	} while (op != 5);
+		//Tambien poder quitar algun ingrediente que el usuario no le guste
+        // combo basico
 
-	Combo* hamburguesa = new Hamburguesa();
+	/*Combo* hamburguesa = new Hamburguesa();
 
 	Combo* queso = new Queso(hamburguesa);
 
@@ -38,7 +94,11 @@ int main() {
 
 	Combo* ppio = lechuga;
 
-	cout << ppio->costo() << " Colones";
+	cout << "Hamburguesa: "<<ppio->costo() << " Colones";
+
+    //Tambien eliminar un ingrediente que no le guste al usuario (Tarea moral)
+
+*/
 
 	system("pause");
 	return 0;
